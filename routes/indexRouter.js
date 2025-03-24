50a5becfaa3fa91915c2ac0b1af0fe75cda68e3f
Moolmancell <richmondclaudebodot@gmail.com>
2025-03-24 21:44:52 +0800

    Display messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const indexRouter = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 indexRouter.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {title: "Mini Messageboard", messages: messages});
 });
 
 module.exports = indexRouter;
